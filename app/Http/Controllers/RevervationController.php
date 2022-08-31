29149cd1309d39880623a4d76a1a43e170a3944f
silasmas <silasjmas@gmail.com>
2022-08-31 15:23:53 +0100

    migration

---
+++ app/Http/Controllers/RevervationController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\revervation;
+use App\Http\Requests\StorerevervationRequest;
+use App\Http\Requests\UpdaterevervationRequest;
+
+class RevervationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StorerevervationRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StorerevervationRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\revervation  $revervation
+     * @return \Illuminate\Http\Response
+     */
+    public function show(revervation $revervation)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\revervation  $revervation
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(revervation $revervation)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdaterevervationRequest  $request
+     * @param  \App\Models\revervation  $revervation
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdaterevervationRequest $request, revervation $revervation)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\revervation  $revervation
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(revervation $revervation)
+    {
+        //
+    }
+}
---
+++ app/Models/revervation.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class revervation extends Model
+{
+    use HasFactory;
+}
